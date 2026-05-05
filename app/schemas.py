81a98b7983655bb26a40f964acd12e3f814d2312
sadra nobari <sadra.nobari83@gmail.com>
2026-05-05 22:18:52 +0330

    basic schema for database

---
+++ app/schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+class TaskCreate(BaseModel):
+    title: str
+    description: Optional[str] = None
+
+class TaskUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    is_completed: Optional[bool] = None
+
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+    description: Optional[str] = None
+    is_completed: bool
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
